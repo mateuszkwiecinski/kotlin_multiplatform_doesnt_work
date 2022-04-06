0b4cbe518361c7160a36126c63b4fc3ebaa0e490
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2022-04-06 20:07:26 +0200

    wip

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    kotlin("multiplatform") version "1.6.20"
+}
+
+kotlin {
+    jvm()
+
+    val iOSFrameworkName = "Foo"
+    val xcf = XCFramework(iOSFrameworkName)
+
+    listOf(
+        iosX64(),
+    ).forEach { target ->
+        target.binaries.framework {
+            baseName = iOSFrameworkName
+            freeCompilerArgs = freeCompilerArgs + listOf("-linker-options", "-application_extension")
+            xcf.add(this)
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting
+
+        val iosX64Main by getting
+
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+        }
+    }
+}
+
+dependencies {
+    commonMainImplementation(kotlin("stdlib-common"))
+    commonTestImplementation(kotlin("test-common"))
+    commonTestImplementation(kotlin("test-annotations-common"))
+}
